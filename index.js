74f6eb567b017a09e3aad69619101a4d72ac5d4c
Ngecha <ngechadennis@gmail.com>
2024-07-08 15:44:14 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const scuberDrivers = ['Antonia','Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = scuberDrivers => scuberDrivers.slice(0,2);
+const returnLastTwoDrivers = function(){
+    return scuberDrivers.slice(2);
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+return function(fare){
+    return multiplier * fare;
+}
+}
+
+const fareDoubler = function(fare){
+    return 2*fare;
+}
+
+const fareTripler = function(fare){
+    return 3*fare;
+}
+
+function selectDifferentDrivers(scuberDrivers,returnFirstTwoDrivers){
+     return returnFirstTwoDrivers (scuberDrivers);
+}
